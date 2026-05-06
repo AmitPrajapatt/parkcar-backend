cbcc2dfcc0d3e306a59d5b36609cda597843d99d
amit prajapat <prajapatamit641@gmail.com>
2026-05-06 18:31:27 +0530

    eror solved 2

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-const Session = require('../models/Session');
+const User = require('../models/user');
+const Session = require('../models/session');
 
 // 1. Get Analytics & Revenue
 // 1. Advanced Analytics & Revenue
